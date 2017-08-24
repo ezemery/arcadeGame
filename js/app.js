c59257bcb37d46a39bc2bd7e5c6abc73b4a8de1f
Emmanuel Eze <emereuwaonueze@gmail.com>
2017-08-24 13:54:41 +0100

    Update player and bug-enemy movement

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 // Enemies our player must avoid
 var Enemy = function(y) {
-    this.x = -50 //Math.floor(Math.random() * (10 + 100)) - 100; // Spawn enemies at different x coordinates between -100 and 10
-    this.y = y//Math.floor(Math.random() * (200 - 20)) + 20;// Spawn enemies at different y coordinates between 20 and 200
-    this.speed = Math.floor(Math.random() * (300 - 50)) + 50; // Spawn enemies with different speeds between 50 and 300
+    this.x = -100;
+    this.y = y;
+    this.speed = Math.floor(Math.random() * (200 - 50)) + 50; // Spawn enemies with different speeds between 50 and 200
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
     
@@ -14,7 +14,12 @@ var Enemy = function(y) {
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    this.x += this.speed * dt ;
+    if(this.x <= 500){
+        this.x += this.speed * dt ;
+    }else{
+        this.x = -50;
+    }
+    
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
@@ -30,14 +35,18 @@ Enemy.prototype.render = function() {
 // a handleInput() method.
 var Player = function(){
     this.x = 200;
-    this.y = 400 ;
+    this.y = 400;
+    this.speed = 50;
     this.sprite = 'images/char-pink-girl.png'
 }
 
 // Update the players's position, required method for game
 // Parameter: dt, a time delta between ticks
 Player.prototype.update = function(dt){
-    
+    if(this.y < 20){
+        this.y = 400;
+        player.render();
+    }
 }
 // Draw the player on the screen, required method for game
 Player.prototype.render = function(){
@@ -46,25 +55,24 @@ Player.prototype.render = function(){
 
 // Input handler for player, required method for game
 Player.prototype.handleInput = function(fn){
-    
-    if(fn == "right" && this.x <= 400){
-        this.x += 20;
+    if(fn == "right" && this.x <= 300){
+        this.x += this.speed;
         player.render();
-    }else if(fn == "left" && this.x >= 0 ){
-        this.x -= 20;
+    }else if(fn == "left" && this.x >= 1 ){
+        this.x -= this.speed;
         player.render();
     }else if(fn == "up" && this.y >= 5 ){
-        this.y -= 20;
+        this.y -= this.speed;
         player.render();
     }
-    else if(fn == "down" && this.y <= 410 ){
-        this.y += 20;
+    else if(fn == "down" && this.y <= 350){
+        this.y += this.speed;
         player.render();
     }
 }
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
-var allEnemies = [new Enemy(30), new Enemy(70), new Enemy(100),new Enemy(140),new Enemy(200)];
+var allEnemies = [new Enemy(60), new Enemy(140),new Enemy(220)];
 // Place the player object in a variable called player
 var player = new Player;
 
